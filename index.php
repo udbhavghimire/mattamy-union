f38539cad962d95f5b3fbc3a032a98bf660ea5df
Udbhav Ghimire <ghimireudbhav@gmail.com>
2024-09-15 21:50:51 +0545

    favicon link

--- index.php
+++ index.php
@@ -35,7 +35,7 @@
     <meta property="og:image" content="https://mattamyunion.ca/union4.jpg">
     <meta property="og:site_name" content="Mattamy Union" />
 
-
+    <link rel="icon" href="/favicon.ico" />
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
